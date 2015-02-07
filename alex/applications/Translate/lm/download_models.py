bac75e4014b487bf3a4b1effac0650708229e566
Ondrej Cifka <ondra@cifka.com>
2015-02-07 15:09:03 +0100

    Add tools for building English ASR model (adapted from PTICS)

---
+++ alex/applications/Translate/lm/download_models.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    import autopath
+
+from alex.utils.config import online_update
+
+if __name__ == '__main__':
+    online_update('applications/PublicTransportInfoCS/lm/final.bg.arpa')
+    online_update('applications/PublicTransportInfoCS/lm/final.tg.arpa')
+    online_update('applications/PublicTransportInfoCS/lm/final.qg.arpa')
+    online_update('applications/PublicTransportInfoCS/lm/final.pg.arpa')
+    online_update('applications/PublicTransportInfoCS/lm/final.dict')
+    online_update('applications/PublicTransportInfoCS/lm/final.dict.sp_sil')
+    online_update('applications/PublicTransportInfoCS/lm/final.vocab')
