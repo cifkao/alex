cfe7cbf6abd0e4349c687c899c45d6c93afec791
Ondrej Cifka <ondra@cifka.com>
2015-02-17 20:43:24 +0100

    Use SpeechTech TTS

--- alex/applications/Translate/slt_hub_en_cs_private.sh
+++ alex/applications/Translate/slt_hub_en_cs_private.sh
@@ -2,7 +2,7 @@
 
 #for A in {1..1000}
 #do
-    python slt_hub.py -c slt_hub_en_cs.cfg ../../resources/private/ext-private.cfg gasr_private.cfg -t vrss_en.cfg vrss_private.cfg
+    python slt_hub.py -c slt_hub_en_cs.cfg ../../resources/private/ext-private.cfg gasr_private.cfg speechtech_private.cfg -t vrss_en.cfg vrss_private.cfg
 #done
 
 
